4f096e680939fbf38c801144faddde6a1fda2196
xial@plian.net <a123456>
2020-11-23 17:18:18 +0800

    init

--- src/pages/input/index9/index.tsx
+++ src/pages/input/index9/index.tsx
@@ -1,17 +1,17 @@
-
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { PageContainer } from '@ant-design/pro-layout';
-import {TableListData, TableListItem} from './data.d';
-import { Form, Select, Button,Card,DatePicker } from 'antd';
-import styles from './style.less';
-import {Constants} from "@/utils/constants";
-import {connect,Dispatch} from "umi";
-import {StateType} from "@/pages/user/login/model";
-import {ColumnProps} from "antd/es/table";
-import {Key, SorterResult, TablePaginationConfig} from "antd/lib/table/interface";
-import {formatFormParams} from "@/utils/common";
-import moment from "moment";
+import { Form, Select, Button, Card, DatePicker } from 'antd';
+import { Constants } from '@/utils/constants';
+import { connect, Dispatch } from 'umi';
+import { StateType } from '@/pages/user/login/model';
+import { ColumnProps } from 'antd/es/table';
+import { Key, SorterResult, TablePaginationConfig } from 'antd/lib/table/interface';
+import { formatFormParams } from '@/utils/common';
+import moment from 'moment';
 import StandardTable from '@/components/StandTable1';
+import { TableListData, TableListItem } from './data.d';
+// @ts-ignore
+import styles from './style.less';
 
 interface TableListProps {
   dispatch: Dispatch;
@@ -19,64 +19,63 @@ interface TableListProps {
   submitting: boolean;
 }
 
-const getValue = (obj:any) =>
+const getValue = (obj: any) =>
   Object.keys(obj)
-    .map(key => obj[key])
+    .map((key) => obj[key])
     .join(',');
 
-const TableList: React.FC<TableListProps> = ({dispatch,submitting}) => {
-  const [height,setHeight] = useState<number>(window.innerHeight-Constants.tableHeight);
-  const [selectedRows,setSelectRows] = useState<TableListItem[]>([]);
-  const [danWeis,setDanWeis] = useState<{compCode:string}[]>([]);
+const TableList: React.FC<TableListProps> = ({ dispatch, submitting }) => {
+  const [height, setHeight] = useState<number>(window.innerHeight - Constants.tableHeight);
+  const [selectedRows, setSelectRows] = useState<TableListItem[]>([]);
+  const [danWeis, setDanWeis] = useState<{ compCode: string }[]>([]);
   const [form] = Form.useForm();
-  const [data,setData] = useState<TableListData>({
-    list:[],
-    pagination:{
-      pageSize:20,
-      current:1,
-      total:0,
-    }
-
+  const [data, setData] = useState<TableListData>({
+    list: [],
+    pagination: {
+      pageSize: 20,
+      current: 1,
+      total: 0,
+    },
   });
-  useEffect(()=>{
-    setHeight(window.innerHeight-Constants.tableHeight)
-  },[height])
+  useEffect(() => {
+    setHeight(window.innerHeight - Constants.tableHeight);
+  }, [height]);
 
-  const list = (params:{[key:string]:any}) =>{
+  const list = (params: { [key: string]: any }) => {
     dispatch({
-      type:'index9/list',
-      payload:params,
-      callback:(response:TableListData)=>{
-        console.log("response",response);
+      type: 'index9/list',
+      payload: params,
+      callback: (response: TableListData) => {
+        console.log('response', response);
         setData(response);
-      }
-    })
-  }
-  const danWei = () =>{
+      },
+    });
+  };
+  const danWei = () => {
     dispatch({
-      type:'common/danWei',
-      callback:(response:{compCode:string}[])=>{
+      type: 'common/danWei',
+      callback: (response: { compCode: string }[]) => {
         setDanWeis(response);
-      }
-    })
-  }
-  useEffect(()=>{
-    list({...formatFormParams(form.getFieldsValue())});
+      },
+    });
+  };
+  useEffect(() => {
+    list({ ...formatFormParams(form.getFieldsValue()) });
     danWei();
-  },[]);
+  }, []);
 
   const columns: ColumnProps<TableListItem>[] = [
     {
       title: '',
       dataIndex: 'id',
-      width:40,
-      fixed:'left',
-      render:(text,record,index9)=><span>{index9+1}</span>
+      width: 40,
+      fixed: 'left',
+      render: (text, record, index9) => <span>{index9 + 1}</span>,
     },
     {
       title: '年度',
       dataIndex: 'acctYear',
-      width:60,
+      width: 60,
     },
     {
       title: '科室编码',
@@ -89,12 +88,12 @@ const TableList: React.FC<TableListProps> = ({dispatch,submitting}) => {
     {
       title: '医生人数',
       dataIndex: 'docNum',
-      width:120,
+      width: 120,
     },
     {
       title: '医生经费',
       dataIndex: 'docWage',
-      width:120,
+      width: 120,
     },
     {
       title: '护士人数',
@@ -103,29 +102,33 @@ const TableList: React.FC<TableListProps> = ({dispatch,submitting}) => {
     {
       title: '护士经费',
       dataIndex: 'nurWage',
-      sorter:true,
-      width:100,
+      sorter: true,
+      width: 100,
     },
     {
       title: '人员经费合计',
       dataIndex: 'nurWage',
-      sorter:true,
-      width:120,
+      sorter: true,
+      width: 120,
     },
   ];
 
-  const onFinish = (values:{[key:string]:any}) => {
+  const onFinish = (values: { [key: string]: any }) => {
     list(formatFormParams(values));
   };
 
-  const handleTableChange=(pagination: TablePaginationConfig, filtersArg: Record<string, Key[] | null>, sorter: SorterResult<TableListItem>)=>{
+  const handleTableChange = (
+    pagination: TablePaginationConfig,
+    filtersArg: Record<string, Key[] | null>,
+    sorter: SorterResult<TableListItem>,
+  ) => {
     const filters = Object.keys(filtersArg).reduce((obj, key) => {
       const newObj = { ...obj };
       newObj[key] = getValue(filtersArg[key]);
       return newObj;
     }, {});
 
-    const params:{[key:string]:any} = {
+    const params: { [key: string]: any } = {
       currentPage: pagination.current,
       pageSize: pagination.pageSize,
       ...formatFormParams(form.getFieldsValue()),
@@ -135,7 +138,7 @@ const TableList: React.FC<TableListProps> = ({dispatch,submitting}) => {
       params.sorter = `${sorter.field}_${sorter.order}`;
     }
     list(params);
-  }
+  };
 
   const handleSelectRows = (rows: TableListItem[]) => {
     setSelectRows(rows);
@@ -144,54 +147,61 @@ const TableList: React.FC<TableListProps> = ({dispatch,submitting}) => {
   return (
     <PageContainer title={false}>
       <div className={styles.search}>
-        <Card bordered={false} size='small' className={styles.searchBar}>
-          <Form form={form} layout="inline" onFinish={onFinish} initialValues={{
-            isCharg:'',
-            compCode:'100001',
-            acctYear:moment('2019'),
-            devCode:"",
-          }}>
-            <Form.Item label='单位' name="compCode">
-              <Select style={{width:100}}>
-                {
-                  danWeis.map(item=>(<Select.Option value={`${item.compCode}`}>{item.compCode}</Select.Option>))
-                }
+        <Card bordered={false} size="small" className={styles.searchBar}>
+          <Form
+            form={form}
+            layout="inline"
+            onFinish={onFinish}
+            initialValues={{
+              isCharg: '',
+              compCode: '100001',
+              acctYear: moment('2019'),
+              devCode: '',
+            }}
+          >
+            <Form.Item label="单位" name="compCode">
+              <Select style={{ width: 100 }}>
+                {danWeis.map((item) => (
+                  <Select.Option value={`${item.compCode}`}>{item.compCode}</Select.Option>
+                ))}
               </Select>
             </Form.Item>
-            <Form.Item label='年度' name="acctYear">
-              <DatePicker format='YYYY' picker="year" />
+            <Form.Item label="年度" name="acctYear">
+              <DatePicker format="YYYY" picker="year" />
             </Form.Item>
-            <Form.Item label='科室名称' name="devCode">
-              <Select style={{width:100}}>
-                <Select.Option value=''>全部</Select.Option>
+            <Form.Item label="科室名称" name="devCode">
+              <Select style={{ width: 100 }}>
+                <Select.Option value="">全部</Select.Option>
               </Select>
             </Form.Item>
             <Form.Item>
               <div className={styles.btns}>
-                <Button type="primary" htmlType="submit">查询</Button>
+                <Button type="primary" htmlType="submit">
+                  查询
+                </Button>
               </div>
             </Form.Item>
           </Form>
         </Card>
       </div>
-      <Card size='small' bordered={false} bodyStyle={{padding:16}}>
+      <Card size="small" bordered={false} bodyStyle={{ padding: 16 }}>
         <StandardTable
           loading={submitting}
           bordered
-          size='small'
-          title={()=>(
+          size="small"
+          title={() => (
             <div className={styles.tableTitle}>
               <span className={styles.title}>人员经费录入</span>
               <div>
-                <Button type='primary'>导入</Button>
-                <Button type='primary'>导出</Button>
-                <Button type='primary'>模版下载</Button>
+                <Button type="primary">导入</Button>
+                <Button type="primary">导出</Button>
+                <Button type="primary">模版下载</Button>
               </div>
             </div>
           )}
           scroll={{
-            x:1200,
-            y:height
+            x: 1200,
+            y: height,
           }}
           columns={columns}
           data={data}
@@ -204,12 +214,11 @@ const TableList: React.FC<TableListProps> = ({dispatch,submitting}) => {
   );
 };
 
-
 export default connect(
   ({
-     index9,
-     loading,
-   }: {
+    index9,
+    loading,
+  }: {
     index9: StateType;
     loading: {
       effects: {
